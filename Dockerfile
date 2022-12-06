675c25809e74258bbe3f3bd679e07f9f62968e2f
Hariharan Subramanian <hari@flowinsights.com>
2022-12-06 18:16:32 +0100

    Solved puzzle 1 by executing move instructions using stacks

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:6.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["SupplyStacks.csproj", "./"]
+RUN dotnet restore "SupplyStacks.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "SupplyStacks.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "SupplyStacks.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "SupplyStacks.dll"]
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Analyzing supply stacks...");
+
+var inputGroups = File.ReadAllText("stacks_input.txt").Split("\r\n\r\n");
+var moveInstructions = inputGroups[1].Split(Environment.NewLine);
+var cratesInput = inputGroups[0].Split(Environment.NewLine);
+var crateNumbersInput = cratesInput.Last();
+
+var labelPositions = new List<int>();
+crateNumbersInput
+    .Select((c, idx) => new {character = c, index = idx})
+    .ToList()
+    .ForEach(c =>
+    {
+        if (char.IsDigit(c.character)) labelPositions.Add(c.index);
+    });
+
+
+// create stacks 
+var crateStacks = new Dictionary<int, Stack>();
+for (var i = 0; i < labelPositions.Count; i++)
+{
+    crateStacks.Add(i + 1, new Stack());
+}
+
+for (var i = 0; i < labelPositions.Count; i++)
+{
+    var alphabetPosition = labelPositions[i];
+    // for each crate row, except the last one, add crate labels to stack
+    for (var j = cratesInput.Length - 1; j >= 0; j--)
+    {
+        var crateRow = cratesInput[j];
+        var crateLabel = crateRow[alphabetPosition];
+        if (char.IsLetter(crateLabel)) crateStacks[i + 1].Push(crateLabel);
+    }
+}
+
+// execute move instructions
+foreach (var moveInstruction in moveInstructions)
+{
+    var moveInstructionParts = Regex.Matches(moveInstruction, @"\d+")
+        .Select(m => m.Value)
+        .ToArray();
+    var transferCount = int.Parse(moveInstructionParts[0]);
+    var fromStack = int.Parse(moveInstructionParts[1]);
+    var toStack = int.Parse(moveInstructionParts[2]);
+
+    // move crates transferCount number of times
+    for (var i = 0; i < transferCount; i++)
+    {
+        var crate = crateStacks[fromStack].Pop();
+        crateStacks[toStack].Push(crate);
+    }
+}
+
+var topOfEachStack = string.Join("", crateStacks.Select(crateStack => (char) (crateStack.Value.Peek() ?? "")).ToList());
+Console.WriteLine(topOfEachStack);
+
+Console.WriteLine($"Finished analyzing supply stacks.");
