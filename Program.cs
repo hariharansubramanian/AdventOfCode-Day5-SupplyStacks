9dbf0543e6950d6f5c557746c005b022e0b49f23
Hariharan Subramanian <hari@flowinsights.com>
2022-12-06 20:33:47 +0100

    Solved puzzle 2 by reversing the stack transfers

--- Program.cs
+++ Program.cs
@@ -5,49 +5,15 @@ Console.WriteLine("Analyzing supply stacks...");
 
 var inputGroups = File.ReadAllText("stacks_input.txt").Split("\r\n\r\n");
 var moveInstructions = inputGroups[1].Split(Environment.NewLine);
-var cratesInput = inputGroups[0].Split(Environment.NewLine);
-var crateNumbersInput = cratesInput.Last();
+var crateRows = inputGroups[0].Split(Environment.NewLine);
+var crateNumberRow = crateRows.Last();
+var crateLabelPositions = GetLabelPositions(crateNumberRow);
+var crateStacks = PopulateCrateStacks(crateLabelPositions, crateRows);
 
-var labelPositions = new List<int>();
-crateNumbersInput
-    .Select((c, idx) => new {character = c, index = idx})
-    .ToList()
-    .ForEach(c =>
-    {
-        if (char.IsDigit(c.character)) labelPositions.Add(c.index);
-    });
-
-
-// create stacks 
-var crateStacks = new Dictionary<int, Stack>();
-for (var i = 0; i < labelPositions.Count; i++)
-{
-    crateStacks.Add(i + 1, new Stack());
-}
-
-for (var i = 0; i < labelPositions.Count; i++)
-{
-    var alphabetPosition = labelPositions[i];
-    // for each crate row, except the last one, add crate labels to stack
-    for (var j = cratesInput.Length - 1; j >= 0; j--)
-    {
-        var crateRow = cratesInput[j];
-        var crateLabel = crateRow[alphabetPosition];
-        if (char.IsLetter(crateLabel)) crateStacks[i + 1].Push(crateLabel);
-    }
-}
-
-// execute move instructions
+// Puzzle 1
 foreach (var moveInstruction in moveInstructions)
 {
-    var moveInstructionParts = Regex.Matches(moveInstruction, @"\d+")
-        .Select(m => m.Value)
-        .ToArray();
-    var transferCount = int.Parse(moveInstructionParts[0]);
-    var fromStack = int.Parse(moveInstructionParts[1]);
-    var toStack = int.Parse(moveInstructionParts[2]);
-
-    // move crates transferCount number of times
+    var (transferCount, fromStack, toStack) = ParseInstructions(moveInstruction);
     for (var i = 0; i < transferCount; i++)
     {
         var crate = crateStacks[fromStack].Pop();
@@ -55,7 +21,75 @@ foreach (var moveInstruction in moveInstructions)
     }
 }
 
-var topOfEachStack = string.Join("", crateStacks.Select(crateStack => (char) (crateStack.Value.Peek() ?? "")).ToList());
-Console.WriteLine(topOfEachStack);
+var topCrates = string.Join("", crateStacks.Select(crateStack => (char) (crateStack.Value.Peek() ?? "")).ToList());
+Console.WriteLine($"Puzzle 1 has crates: {topCrates} at the top");
 
-Console.WriteLine($"Finished analyzing supply stacks.");
+// Puzzle 2
+crateStacks.Clear();
+crateStacks = PopulateCrateStacks(crateLabelPositions, crateRows);
+
+// execute move instructions
+foreach (var moveInstruction in moveInstructions)
+{
+    var (transferCount, fromStack, toStack) = ParseInstructions(moveInstruction);
+    // Reverse move crates transferCount number of times
+    crateStacks[fromStack].ToArray()
+        .Take(transferCount)
+        .Reverse().ToList()
+        .ForEach(supply =>
+        {
+            crateStacks[fromStack].Pop();
+            crateStacks[toStack].Push(supply);
+        });
+}
+
+topCrates = string.Join("", crateStacks.Select(crateStack => (char) (crateStack.Value.Peek() ?? "")).ToList());
+Console.WriteLine($"Puzzle 2 has crates: {topCrates} at the top");
+
+Console.WriteLine("Finished analyzing supply stacks.");
+
+// functions
+
+(int transferCount, int fromStack, int toStack) ParseInstructions(string instruction)
+{
+    var instructionParts = Regex.Matches(instruction, @"\d+")
+        .Select(m => m.Value)
+        .ToArray();
+    var transferCount = int.Parse(instructionParts[0]);
+    var fromStack = int.Parse(instructionParts[1]);
+    var toStack = int.Parse(instructionParts[2]);
+    return (transferCount, fromStack, toStack);
+}
+
+Dictionary<int, Stack> PopulateCrateStacks(IReadOnlyList<int> labelPositions, IReadOnlyList<string> supplyCratesInput)
+{
+    var crateStacks = new Dictionary<int, Stack>();
+    for (var i = 0; i < labelPositions.Count; i++) crateStacks.Add(i + 1, new Stack());
+
+    for (var i = 0; i < labelPositions.Count; i++)
+    {
+        var alphabetPosition = labelPositions[i];
+        // for each crate row, except the last one, add crate labels to stack
+        for (var j = supplyCratesInput.Count - 1; j >= 0; j--)
+        {
+            var crateRow = supplyCratesInput[j];
+            var crateLabel = crateRow[alphabetPosition];
+            if (char.IsLetter(crateLabel)) crateStacks[i + 1].Push(crateLabel);
+        }
+    }
+
+    return crateStacks;
+}
+
+List<int> GetLabelPositions(string row)
+{
+    var positions = new List<int>();
+    row
+        .Select((c, idx) => new {character = c, index = idx})
+        .ToList()
+        .ForEach(c =>
+        {
+            if (char.IsDigit(c.character)) positions.Add(c.index);
+        });
+    return positions;
+}
